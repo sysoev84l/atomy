9c6cefb60f376822ace28d9899db194ed3a0245b
sysoev84l <sysoev84l@yandex.ru>
2026-02-23 11:33:50 +0300

    v 0.6.3

--- assets/php/add-data.php
+++ assets/php/add-data.php
@@ -1,11 +1,13 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . "/" . "assets/php/functions.php";
-if (get_server_name() == 'http://f1230728.xsph.ru/') require_once $_SERVER['DOCUMENT_ROOT'] . "/assets/php/connect_hosting.php";
-    else require_once $_SERVER['DOCUMENT_ROOT'] . "/assets/php/connect.php";
+if (get_server_name() == 'http://f1230728.xsph.ru')
+	require_once $_SERVER['DOCUMENT_ROOT'] . "/assets/php/connect_hosting.php";
+else
+	require_once $_SERVER['DOCUMENT_ROOT'] . "/assets/php/connect.php";
 if (isset($_POST) && !empty($_POST)) {
 	$date = $_POST['date'];
 	$type = $_POST['type'];
-	$quantity = $_POST['quantity'];	
+	$quantity = $_POST['quantity'];
 	$sql = "INSERT INTO `pallet_accouting` (`date`, `type`, `quantity`) VALUES ('$date', '$type', '$quantity')";
 	$result = $mysqli->query($sql);
 	header("Location: " . get_server_name());
